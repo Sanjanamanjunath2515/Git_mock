b47f0a334fb82c88e1ed6438097b043ff490aab7
Sanjanamanjunath2515 <sanjanamanjunath2515@gmail.com>
2026-08-29 16:50:40 +0530

    Added two files in dev

---
+++ Devfile1.java
@@ -0,0 +1,5 @@
+public class DevFile1 {
+    public static void main(String[] args) {
+        System.out.println("Hello from Dev File 1");
+    }
+}
---
+++ Devfile2.java
@@ -0,0 +1,5 @@
+public class DevFile2 {
+    public static void main(String[] args) {
+        System.out.println("Hello from Dev File 2");
+    }
+}
